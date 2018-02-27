4825980ee47217056134849d63614c5142803bbe
xiaocj <xiaocj16@mails.tsinghua.edu.cn>
2018-02-27 19:34:47 +0800

    train crf

--- crf/train.py
+++ crf/train.py
@@ -40,6 +40,7 @@ def train(data, taskName):
     trainer.set_params({
         'c1': 1.0,  # coefficient for L1 penalty
         'c2': 1e-3,  # coefficient for L2 penalty
+
         'max_iterations': 100,  # stop earlier
         # include transitions that are possible, but not observed
         'feature.possible_transitions': True
